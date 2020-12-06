f289799b505494507d4f418370127e7130d7536c
Rui Costa <ruicosta042@gmail.com>
2020-12-06 21:51:29 +0000

    feat: remove docker related files

--- sh/build.sh
+++ sh/build.sh
@@ -4,7 +4,9 @@
 rimraf dist
 
 # build library
-microbundle --output dist/index.js  --no-pkg-main --target node --sourcemap false --format cjs
-
-# docker distribution files
-cp -r docker dist
+microbundle \
+  --output dist/index.js  \
+  --no-pkg-main \
+  --target node \
+  --sourcemap false \
+  --format cjs
