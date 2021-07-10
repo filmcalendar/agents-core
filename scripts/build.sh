259fafafb4c09110fa104db738054ad4a973afa3
Rui Costa <ruicosta042@gmail.com>
2021-07-10 01:57:14 +0100

    feat: include cjs/esm in package

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,14 @@ tsc --build tsconfig.build.json
 esbuild src/index.ts \
   --bundle \
   --platform=node \
+  --outfile=dist/index.cjs
+
+esbuild src/index.ts \
+  --bundle \
+  --platform=node \
   --format=esm \
-  --outfile=dist/index.js
+  --outfile=dist/index.mjs
+
 
 cp src/agents-core.d.ts dist
 cp dist/agent/index.d.ts dist/agent.d.ts
